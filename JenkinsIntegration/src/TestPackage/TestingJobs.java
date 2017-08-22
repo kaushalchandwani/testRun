584cbdb222c81b07ea5675a0635e1d1177a4bcce
kaushalchandwani <kaushal.chandwani@nhs.net>
2017-08-22 16:22:04 +0100

    added test

---
+++ JenkinsIntegration/src/TestPackage/TestingJobs.java
@@ -0,0 +1,15 @@
+package TestPackage;
+
+import org.testng.annotations.Test;
+
+
+
+public class TestingJobs 
+{
+	@Test
+	public static void testJenkins()
+	{
+		System.out.println("Welcome to jenkins World");
+	}
+
+}
